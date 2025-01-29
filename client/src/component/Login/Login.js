b5f147ba919bdc100fd088e378ae5d287ff79d7f
matt styles <styles.matt135@gmail.com>
2025-01-28 20:54:57 -0500

    Console Reading Login Data

--- client/src/component/Login/Login.js
+++ client/src/component/Login/Login.js
@@ -5,30 +5,31 @@ import axios from 'axios'
 
 export default function Login({user, setUser}) {
     const [formData, setFormData] = useState ({
-      user_name: "",
+      user_email: "",
       user_password: ""
     })
   
     const handleInput = (event) => {
-      const name = event.target.user_name
+      const name = event.target.name
       const value = event.target.value
       setFormData ({...formData, [name]: value})
     }
 
     const handleSubmit = async (event) => {
         event.preventDefault()
+        console.log (formData)
         try {
           const response = await axios.get ("http://localhost:3002/users/",{
           params: {
-            user_name: formData.user_name,
+            user_email: formData.user_email,
             user_password: formData.user_password,
           },})
     
-          
+          console.log (response)
           if ( response.data.length > 0 ) {        
             setUser((prevUser) => {
               const DBuser = response.data[0]
-              const updatedUser = { ...prevUser, id: DBuser.id , user_name: DBuser.user_name};
+              const updatedUser = { ...prevUser, id: DBuser.id , user_email: DBuser.user_email};
               console.log("Updated user:", updatedUser); 
               return updatedUser;
             });
@@ -45,16 +46,16 @@ export default function Login({user, setUser}) {
             <h2>Login</h2>
             <form action="login" onSubmit={handleSubmit}>
                 <div classname="mb-3">
-                    <label htmlFor= "email"><strong>Email</strong></label>
-                    <input type="email" placeholder='Enter Email' name='email'
+                    <label htmlFor= "user_email"><strong>Email</strong></label>
+                    <input type="username" placeholder='Enter Your Email' name='user_email'
                     onChange={handleInput} className='form-control rounded-0'/>
                 </div>
                 <div classname="mb-3">
-                    <label htmlFor= "password"><strong>Password</strong></label>
-                    <input type="password" placeholder='Enter Password' name= 'password'
+                    <label htmlFor= "user_password"><strong>Password</strong></label>
+                    <input type="password" placeholder='Enter Password' name= 'user_password'
                     onChange={handleInput} className='form-control rounded-0'></input>
                 </div>
-                <button type='submit' className='btn btn-success w-100'>Login</button>
+                <button type='submit' className='btn btn-success w-100' onClick={handleSubmit}>Login</button>
                 <Link to="/signup" className='btn btn-default border w-100 bg-light rounded-0 text-decoration-none'>Create Account</Link>
             </form>
             </div>
